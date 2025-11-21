d32ad8b6463be4a87720a22b1a40b5adcfdb0061
ariaaruasaru-prog <ariaaruasaru@gmail.com>
2025-11-21 13:55:17 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,76 @@
+#  numbers pyramid 
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print (j,end=" " )
+        
+        
+    
+    print()    
+   
+# basic calculator programe
+
+
+# def add(a,b):
+#     return a+b
+
+# def sub(a,b):
+#     return a-b
+
+# def mul(a,b):
+#     return a*b
+
+# def div(a,b):
+#     return a/b
+
+# def modules(a,b):
+#     return a % b
+
+# def floor(a,b):
+#     return a//b
+
+# def main():
+#     while True:
+#         print("basic calculator")
+#         print('1.Addition \n 2.substraction \n 3.multiplication \n 4.division \n 5.modules \n 6.floor division \n 7.Exit')
+        
+#         ch=int(input("enter your choice: "))
+        
+#         a=int(input("enter your number: "))
+#         b=int(input("enter your number: "))
+
+#         if ch==1:
+#             print(f'sum of {a} and {b} is {add(a,b)}.')
+
+#         elif ch==2:
+#             print(f'difference of {a} and {b} is {sub(a,b)}.') 
+
+#         elif ch==3:
+#             print(f'product of {a} and {b} is {mul(a,b)}.')
+
+#         elif ch==4:
+#             print(f'quotient of {a} and {b} is {div(a,b)}.')
+
+#         elif ch==5:
+#             print(f'The modulo of {a} and {b} is {modules(a,b)}.')
+
+#         elif ch==6:
+#             print(f'The round it down value of {a} and {b} is {floor(a,b)}.') 
+        
+#         elif ch==7:
+#             break
+#         else:
+#             print(" invalid choice")      
+# main()                            
+
+
+    
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print (j,end=" " )
+        
+        
+    
+    print()    
+   
